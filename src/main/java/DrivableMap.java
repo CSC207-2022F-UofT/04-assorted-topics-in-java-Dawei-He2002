d268279a2c863375cfb978a5494363abb55cbbd7
Dawei-He2002 <dawei.he@mail.utoronto.ca>
2022-10-01 22:20:00 -0400

    All methods completed. TODOs removed to save space.

--- src/main/java/DrivableMap.java
+++ src/main/java/DrivableMap.java
@@ -22,32 +22,43 @@ class DrivableMap {
         drivable_map = new HashMap<>();
     }
 
-    /* TODO: Write a method named addDrivable that takes a String (the ID)
-     *       and a Drivable object. If the ID string does not appear as a key
-     *       in drivable_map, then add the pair to drivable_map.
-     *       Return true if the Drivable was added to drivable_map.
-     */
+    public boolean addDrivable(String ID, Drivable obj){
+        if(drivable_map.containsKey(ID)){
+            return false;
+        }
+        else {
+            drivable_map.put(ID, obj);
+            return true;
+        }
+    }
 
 
 
+    public boolean hasFasterThan(int speed) {
+        for (String i : drivable_map.keySet()) {
+            if (drivable_map.get(i) instanceof Drivable){
+                if (( drivable_map.get(i)).getMaxSpeed() >= speed){
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
 
-    /* TODO: Write a method named hasFasterThan that takes an int (a speed)
-     *       and returns true iff there is at least one item in drivable_map
-     *       that has a maxSpeed >= the speed given.
-     * You may want to use drivable_map.keys() or drivable_map.values() to
-     * iterate through drivable_map.
-     */
 
 
-
-
-
-    /* TODO: Write a method named getTradable that takes no arguments and
-     *       returns a List containing all of the Tradable items in
-     *       drivable_map.
-     */
+    public List getTradable(){
+        List temp;
+        temp = new ArrayList();
+        for (String i : drivable_map.keySet()) {
+            if (drivable_map.get(i) instanceof Tradable){
+                temp.add(drivable_map.get(i));
+                }
+            }
+        return temp;
+        }
+    }
 
 
 
     
-}
